43afd0f61e2872faa2bf8e5ad112b4c5a4cfe7e8
Katie Wetstone <klwetstone@gmail.com>
2024-08-16 18:16:37 -0400

    correct indices

--- examples/submission/main.py
+++ examples/submission/main.py
@@ -10,12 +10,12 @@ SUBMISSION_FORMAT_PATH = Path("data/submission_format.csv")
 
 
 def main():
-    features = pd.read_csv(FEATURES_PATH)
+    features = pd.read_csv(FEATURES_PATH, index_col=0)
     print(f"Loaded test features of shape {features.shape}")
 
-    sub_format = pd.read_csv(SUBMISSION_FORMAT_PATH)
+    sub_format = pd.read_csv(SUBMISSION_FORMAT_PATH, index_col=0)
 
-    predictions = pd.DataFrame({"uid": features.uid})
+    predictions = pd.DataFrame({"uid": features.index})
     for col in sub_format.columns:
         predictions[col] = 1
     predictions["WeaponType1"] = "Unknown"
